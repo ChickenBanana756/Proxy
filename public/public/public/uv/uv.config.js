83b3aefdd0f62c7d8e70b90348a1f24be4df3b37
ChickenBanana756 <drparibansari@gmail.com>
2026-01-31 09:56:38 -0600

    Create uv.config.js

---
+++ public/public/public/uv/uv.config.js
@@ -0,0 +1,8 @@
+self.__uv$config = {
+  prefix: "/service/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/uv/uv.bundle.js",
+  client: "/uv/uv.bundle.js",
+  bundle: "/uv/uv.bundle.js"
+};
