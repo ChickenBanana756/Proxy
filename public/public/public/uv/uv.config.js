200e78271c8ead9eb34659a126a282bfc9acf020
ChickenBanana756 <drparibansari@gmail.com>
2026-01-31 10:11:22 -0600

    Update uv.config.js

--- public/public/public/uv/uv.config.js
+++ public/public/public/uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
   prefix: "/service/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.bundle.js",
-  client: "/uv/uv.bundle.js",
-  bundle: "/uv/uv.bundle.js"
+  handler: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.bundle.js",
+  client: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.bundle.js",
+  bundle: "https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.bundle.js"
 };
