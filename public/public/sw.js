2ad9c144a3d5b5f159c579d768e61928c59c0bb5
ChickenBanana756 <drparibansari@gmail.com>
2026-01-31 10:10:54 -0600

    Update sw.js

--- public/public/sw.js
+++ public/public/sw.js
@@ -1,4 +1,5 @@
-importScripts("/uv/uv.bundle.js");
+// Use CDN for uv.bundle.js
+importScripts("https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet/dist/uv.bundle.js");
 importScripts("/uv/uv.config.js");
 
 const uv = new UVServiceWorker();
